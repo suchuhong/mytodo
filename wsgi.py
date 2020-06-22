174600dd65691f29fefbf9cfffae363902366f03
suchuhong <1442615457@qq.com>
2020-06-23 00:09:01 +0800

    新增 nginx 的配置方案

--- wsgi.py
+++ wsgi.py
@@ -18,6 +18,17 @@ application = app.app
 
 # 这是把代码部署到 apache gunicorn nginx 后面的套路
 """
+配置nginx
+mkdir /etc/nginx/sites-available
+mkdir /etc/nginx/sites-enabled
+
+修改nginx配置文件 /etc/nginx/nginx.conf 使其包含符号链接虚拟主机文件，在 http {} 区块结束前加上如下内容：
+include /etc/nginx/sites-enabled/*.nginx;
+
+配置软链接到 /etc/nginx/sites-available  /etc/nginx/sites-enabled
+systemctl restart nginx
+
+
 配置方法已经更改 centos7安装supervisor详细教程 https://www.jianshu.com/p/8605fcdb1138
 gunicorn wsgi --bind 0.0.0.0:2000
 ➜  ~ cat /etc/supervisor/conf.d/xx.conf
