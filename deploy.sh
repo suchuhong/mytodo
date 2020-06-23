f00fa59b05e69f18a2482d3e139f351e6895b156
suchuhong <1442615457@qq.com>
2020-06-23 15:47:32 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 ln -s ~/var/www/mytodo/config/mytodo.conf /etc/supervisord.d/mytodo.conf
-ln -s ~/var/www/mytodo/config/mytodo.nginx /etc/nginx/sites-enabled/mytodo
+ln -s ~/var/www/mytodo/config/mytodo.nginx /etc/nginx/sites-available/mytodo.nginx
+ln -s /etc/nginx/sites-available/mytodo.nginx /etc/nginx/sites-enabled/mytodo.nginx
